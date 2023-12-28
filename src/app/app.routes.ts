6526906018b0c457a40c9e8d88292e409ec6dd9b
valerioprds <v.paradiso89@gmail.com>
2023-12-28 16:28:22 +0100

    generated delete component and implemented routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { DeleteComponent } from './delete/delete.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: '', component: MainComponent },
+  { path: 'delete', component: DeleteComponent },
+
+
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+
+];
