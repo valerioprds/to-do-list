cbaba640409ca16b9c16fa297213013d29c0e062
valerioprds <v.paradiso89@gmail.com>
2023-12-28 10:39:31 +0100

    to do list now works,

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { MainComponent } from "./main/main.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, HeaderComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, MainComponent]
 })
 export class AppComponent {
   title = 'to-do-list';
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-main',
-    standalone: true,
-    templateUrl: './main.component.html',
-    styleUrl: './main.component.scss',
-    imports: [HeaderComponent]
+  selector: 'app-main',
+  standalone: true,
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss',
+  imports: [HeaderComponent, ReactiveFormsModule, CommonModule],
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  todoForm!: FormGroup;
+  todos: any[] = [];
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.todoForm = this.formBuilder.group({
+      taskTitle: ['', Validators.required],
+    });
+  }
+
+  addTodo() {
+    const taskControl = this.todoForm.get('taskTitle');
+
+    if (taskControl && taskControl.value) {
+      this.todos.push({
+        title: taskControl.value,
+        isCompleted: new FormControl(false),
+      });
+      this.todoForm.reset();
+    }
+  }
 }
