32e39587b0c54f738f9071418e4b74c023a67e5f
valerioprds <v.paradiso89@gmail.com>
2023-12-29 15:58:49 +0100

    teasting header ok

--- README.md
+++ README.md
@@ -25,3 +25,11 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+
+
+npm run test -- header.component.spec.ts
+
+
+how to install the app 
+how to run tests
--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,23 +1,49 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let router: Router;
 
   beforeEach(async () => {
+    const routerMock = {
+      navigate: jest.fn(),
+    };
+
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
-    })
-    .compileComponents();
-    
+      imports: [HeaderComponent],
+      providers: [{ provide: Router, useValue: routerMock }],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to the root route on showPendingTasks', () => {
+    component.showPendingTasks();
+    expect(router.navigate).toHaveBeenCalledWith(['']);
+  });
+
+  it('should navigate to /deleted on showDeleted', () => {
+    component.showDeleted();
+    expect(router.navigate).toHaveBeenCalledWith(['/deleted']);
+  });
+
+  it('should navigate to /all on showAlltasks', () => {
+    component.showAlltasks();
+    expect(router.navigate).toHaveBeenCalledWith(['/all']);
+  });
+
+  it('should navigate to /completed on showCompletedTasks', () => {
+    component.showCompletedTasks();
+    expect(router.navigate).toHaveBeenCalledWith(['/completed']);
   });
 });
