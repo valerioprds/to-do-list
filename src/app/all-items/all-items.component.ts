7d86163565ab128b97f4402ec97586823d137a63
valerioprds <v.paradiso89@gmail.com>
2023-12-29 09:54:09 +0100

    generated all-items

---
+++ src/app/all-items/all-items.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-items.component.html',
+  styleUrl: './all-items.component.scss'
+})
+export class AllItemsComponent {
+
+}
