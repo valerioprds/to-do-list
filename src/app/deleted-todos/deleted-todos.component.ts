4d7c668369366676b9f89e4beaff2e952500fedb
valerioprds <v.paradiso89@gmail.com>
2023-12-29 09:49:40 +0100

    fixed styling of deleted ul

--- src/app/deleted-todos/deleted-todos.component.ts
+++ src/app/deleted-todos/deleted-todos.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Todo } from '../../models/todo';
 import { TodoService } from '../services/todo.service';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
-  selector: 'app-deleted-todos',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './deleted-todos.component.html',
-  styleUrl: './deleted-todos.component.scss'
+    selector: 'app-deleted-todos',
+    standalone: true,
+    templateUrl: './deleted-todos.component.html',
+    styleUrl: './deleted-todos.component.scss',
+    imports: [CommonModule, HeaderComponent]
 })
 export class DeletedTodosComponent  implements OnInit {
   constructor(private todoService: TodoService) {}
