3fd69211fc6426b22e57bfb42ae9645211bcd6b7
valerioprds <v.paradiso89@gmail.com>
2023-12-29 16:37:40 +0100

    testing for completed is now ok

--- src/app/completed-todos/completed-todos.component.spec.ts
+++ src/app/completed-todos/completed-todos.component.spec.ts
@@ -1,17 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CompletedTodosComponent } from './completed-todos.component';
+import { TodoService } from '../services/todo.service'; // Adjust the import path accordingly
 
 describe('CompletedTodosComponent', () => {
   let component: CompletedTodosComponent;
   let fixture: ComponentFixture<CompletedTodosComponent>;
+  let mockTodoService: Partial<TodoService>; // Create a partial mock of TodoService
 
   beforeEach(async () => {
+    // Initialize the mock with necessary service methods
+    mockTodoService = {
+      getCompletedTodos: jest.fn().mockReturnValue([
+        { id: 1, title: 'Completed Task 1', isCompleted: true },
+        { id: 2, title: 'Completed Task 2', isCompleted: true }
+      ])
+      // Add other methods if needed
+    };
+
     await TestBed.configureTestingModule({
-      imports: [CompletedTodosComponent]
+      imports: [CompletedTodosComponent],
+      providers: [
+        { provide: TodoService, useValue: mockTodoService }
+      ]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(CompletedTodosComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +33,21 @@ describe('CompletedTodosComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display completed tasks', () => {
+    // Trigger change detection to update the view
+    fixture.detectChanges();
+
+    // Assuming your component template renders completed tasks in a certain way
+    // For example, you might have an element with class '.completed-task' for each task
+    const taskElements = fixture.nativeElement.querySelectorAll('.completed-task');
+    expect(taskElements.length).toBe(2);
+  });
+
+  it('should use TodoService to get completed tasks', () => {
+    // Verify that the service method is called
+    expect(mockTodoService.getCompletedTodos).toHaveBeenCalled();
+  });
+
+  // Add more tests as necessary based on the functionality of your component
 });
